e998797e68b4f898015fc5a14271c1927d427eee
Rayan LOULIZI <rloulizi@e1r8p16.42.fr>
2017-12-17 16:12:27 +0100

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rloulizi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/09 17:40:20 by rloulizi          #+#    #+#             */
+/*   Updated: 2017/05/16 18:48:23 by rloulizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+# include "libft/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
